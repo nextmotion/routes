5d078d9cc2cbc924982fc18bb6d5e0d61e3c2f9c
nm-dh <88541923+nm-dh@users.noreply.github.com>
2023-11-30 12:07:45 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,9 @@ $EM_CONF['routes'] = [
     'author' => 'Borulko Serhii',
     'author_email' => 'borulkosergey@icloud.com',
     'state' => 'beta',
-    'clearCacheOnLoad' => true,
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.99',
             'typo3' => '12.2.0-12.6.99'
         ]
     ]
